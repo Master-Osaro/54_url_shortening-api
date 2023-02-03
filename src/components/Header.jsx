a52cb97f069114813728586cc87ed2bc003d8d55
Osaro Iyoha <saro.iyoha@gmail.com>
2023-02-03 15:44:55 +0100

    Styling Mobile Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
 import logo from '../images/logo.svg'
+import menuIcon from '../images/icon-hamburger.svg'
+import { useState } from 'react'
 const Header=()=>{
+    const [navOpen, setNavOpen] = useState(false)
     return <header>
             <nav>
                 <div className="nav__group">
                     <img src={logo} alt="" className="logo" />
-                    <div className="nav-list">
-                        <a href="" className="nav-link">Features</a>
-                        <a href="" className="nav-link">Pricing</a>
-                        <a href="" className="nav-link">Resources</a>
+                    <div className={`nav-list__wrapper ${navOpen===false?'hide':''}`}>
+                        <div className="nav-list">
+                            <a href="" className="nav-link">Features</a>
+                            <a href="" className="nav-link">Pricing</a>
+                            <a href="" className="nav-link">Resources</a>
+                        </div>
+                        <hr className="divider" />
+                        <div className="nav-list">
+                            <a href="" className="nav-link">Login</a>
+                            <button className='btn btn-primary'>Sign Up</button>
+                        </div>
                     </div>
                 </div>
                 
-                <div className="nav__btns">
-                    <a href="" className="nav-link">Login</a>
-                    <button className='btn btn-primary'>signup</button>
-                    <button className='menu'>Menu</button>
+                <div className="nav-menu__wrapper">
+                    <button className='menu'onClick={()=>{
+                        setNavOpen(!navOpen)
+                    }}><img src={menuIcon} alt="" /></button>
                 </div>
             </nav>
         </header>
